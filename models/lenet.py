08eb5db3787c855ef8d980b8dd1dabeb2732374c
ciodar <dariocioni03@gmail.com>
2023-02-05 16:05:51 +0100

    Activation choice for lenet

--- models/lenet.py
+++ models/lenet.py
@@ -4,23 +4,25 @@ import torch.nn.functional as F
 
 
 class LeNet300(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, activation=F.relu):
         super().__init__()
         self.fc1 = nn.Linear(28*28, 300)
         self.fc2 = nn.Linear(300, 100)
         self.fc3 = nn.Linear(100, num_classes)
+        # activation function for hidden layers
+        self.activation = activation
 
     def forward(self, x):
         out = torch.flatten(x, 1)
-        out = F.relu(self.fc1(out))
-        out = F.relu(self.fc2(out))
+        out = self.activation(self.fc1(out))
+        out = self.activation(self.fc2(out))
         out = self.fc3(out)
         return out
 
 
 # Defining the convolutional neural network
 class LeNet5(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, activation=F.relu):
         super().__init__()
         self.layer1 = nn.Sequential(
             nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
@@ -33,18 +35,18 @@ class LeNet5(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.fc = nn.Linear(400, 120)
-        self.relu = nn.ReLU()
         self.fc1 = nn.Linear(120, 84)
-        self.relu1 = nn.ReLU()
         self.fc2 = nn.Linear(84, num_classes)
+        # activation function for hidden layers
+        self.activation = activation
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
         out = out.reshape(out.size(0), -1)
         out = self.fc(out)
-        out = self.relu(out)
+        out = self.activation(out)
         out = self.fc1(out)
-        out = self.relu1(out)
+        out = self.activation(out)
         out = self.fc2(out)
         return out
