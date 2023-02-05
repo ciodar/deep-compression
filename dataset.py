f9e463684a0d4f093eddcf9b8875266ca3a0c986
ciodar <dariocioni03@gmail.com>
2023-02-05 16:06:25 +0100

    Validation for mnist

--- dataset.py
+++ dataset.py
@@ -2,7 +2,7 @@ import json
 import os
 
 from PIL import Image
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, SubsetRandomSampler,DataLoader
 import torchvision
 import torchvision.transforms as transforms
 import torch
@@ -10,13 +10,14 @@ import torch
 DATA_DIR = os.path.dirname(os.path.abspath(__file__)) + '/data'
 
 
-def get_mnist_loader(batch_size, num_workers=2, resize=False):
+def get_mnist_loader(batch_size, num_workers=2, val_fraction=None, resize=False):
     # Load datasets
 
     trainset = torchvision.datasets.MNIST(root=DATA_DIR, train=True,
                                           download=True)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=num_workers)
+
+
+
 
     mean = trainset.data.float().mean() / 255
     std = trainset.data.float().std() / 255
@@ -31,15 +32,47 @@ def get_mnist_loader(batch_size, num_workers=2, resize=False):
     transform = transforms.Compose(ts)
     trainset.transform = transform
 
+    validset = torchvision.datasets.MNIST(root=DATA_DIR, train=True,
+                                          transform=transform)
+
     testset = torchvision.datasets.MNIST(root=DATA_DIR, train=False,
                                          download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+
+    # print((len(trainset.data) + len(testset.data)))
+
+    if val_fraction is not None:
+        num = int(val_fraction * (len(trainset.data) + len(testset.data)))
+        train_indices = torch.arange(0, 60000 - num)
+        valid_indices = torch.arange(60000 - num, 60000)
+
+        train_sampler = SubsetRandomSampler(train_indices)
+        valid_sampler = SubsetRandomSampler(valid_indices)
+
+        valid_loader = DataLoader(dataset=validset,
+                                  batch_size=batch_size,
+                                  num_workers=num_workers,
+                                  sampler=valid_sampler)
+
+        train_loader = DataLoader(dataset=trainset,
+                                  batch_size=batch_size,
+                                  num_workers=num_workers,
+                                  drop_last=True,
+                                  sampler=train_sampler)
+
+    train_loader = DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=num_workers)
+
+    test_loader = DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=num_workers)
 
-    return trainloader, testloader
+
+    if val_fraction is None:
+        return train_loader, test_loader
+    else:
+        return train_loader,valid_loader,test_loader
 
 
-def get_cifar100_loader(batch_size, num_workers=2):
+def get_cifar100_loader(batch_size, num_workers=2, val_fraction=None):
     # Load datasets
     trainset = torchvision.datasets.CIFAR100(root=DATA_DIR, train=True,
                                              download=True)
