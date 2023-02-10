cf186fd343551e624baf6a37ee83e2b37cdd4d2c
ciodar <dariocioni03@gmail.com>
2023-02-10 09:30:56 +0100

    Refactored training process

--- train.py
+++ train.py
@@ -10,152 +10,200 @@ Models:
     https://codeberg.org/ciodar/model-compression/src/branch/master/models
 """
 
-
-import time, os
+import time, os, datetime
 import argparse
-import datetime
+import pathlib as pl
+import tqdm
 
 import torch
-from torch.utils.tensorboard import SummaryWriter
-import torchvision.transforms as transforms
-import torch.nn.functional as F
 import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
 
+import optuna
+
+from utils import plot_classes_preds, weight_histograms
 from dataset import get_mnist_loader
 from utils import plot_classes_preds, weight_histograms, set_all_seeds
-from models.lenet import LeNet5,LeNet300
-
+from models.lenet import LeNet5, LeNet300
 
 CHECKPOINT_DIR = os.path.dirname(os.path.abspath(__file__)) + '/checkpoints'
 RUNS_DIR = os.path.dirname(os.path.abspath(__file__)) + '/runs'
 
+
+class AverageMeter(object):
+    """Computes and stores the average and current value
+    https://github.com/pytorch/examples/blob/1de2ff9338bacaaffa123d03ce53d7522d5dcc2e/imagenet/main.py
+    """
+
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
 def training_loop(epochs, model, optimizer, device, train_loader, valid_loader, loss_fn, logging_interval=100,
-                  scheduler=None, checkpoint_path=None, writer=None):
+                  scheduler=None, checkpoint_path=None, writer=None, opt_trial=None):
     start_time = time.time()
-    minibatch_loss_list, train_acc_list, valid_acc_list = [], [], []
     best_valid_acc, best_epoch = -float('inf'), 0
-    running_loss = 0.0
     for epoch in range(epochs):
         model.train()
-        for i_batch, (inputs, labels) in enumerate(train_loader):
+        train_loss, train_acc1, train_acc5 = train_epoch(model, optimizer, device, train_loader, loss_fn,
+                                                         logging_interval)
+
+        if writer:
+            writer.add_scalar('Loss/train', train_loss, epoch)
+            writer.add_scalar('Acc@1/train', train_acc1, epoch)
+            writer.add_scalar('Acc@5/train', train_acc5, epoch)
+
+        valid_loss, valid_acc1, valid_acc5 = evaluate(model, valid_loader, device, loss_fn)
+
+        if valid_acc1 > best_valid_acc:
+            best_valid_acc, best_epoch = valid_acc1, epoch + 1
+            if checkpoint_path:
+                torch.save(model.state_dict(), checkpoint_path)
+
+        if writer:
+            writer.add_scalar('Loss/test', valid_loss, epoch)
+
+            writer.add_scalar('Acc@1/test', valid_acc1, epoch)
+            writer.add_scalar('Acc@5/test', valid_acc5, epoch)
+            # writer.add_figure('predictions vs. actuals',
+            #                  plot_classes_preds(model, inputs[:4], labels[:4], classes),
+            #                  epoch)
+            weight_histograms(writer, epoch, model)
+
+        print(f'Epoch: {epoch + 1:03d}/{epochs:03d} '
+              f'| Train accuracy: {train_acc1 :.2f}% '
+              f'| Validation accuracy: {valid_acc1 :.2f}% '
+              f'| Train loss: {train_loss :.3f}'
+              f'| Validation loss: {valid_loss :.3f}'
+              f'| Best Validation '
+              f'(Ep. {best_epoch:03d}): {best_valid_acc :.2f}%')
+
+        if opt_trial:
+            opt_trial.report(valid_acc1, epoch)
+            # Handle pruning based on the intermediate value.
+            if opt_trial.should_prune():
+                raise optuna.exceptions.TrialPruned()
+
+        if scheduler is not None:
+            scheduler.step()
+
+    print('Total Training Time: %.2f min' % ((time.time() - start_time) / 60))
+    return best_valid_acc
+
+
+def train_epoch(model, optimizer, device, loader, loss_fn, logging_interval=100):
+    start_time = time.time()
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    model.train()
+    with tqdm.tqdm(total=len(loader)) as pbar:
+        for i_batch, (inputs, labels) in enumerate(loader):
             inputs = inputs.to(device)
             labels = labels.to(device)
 
             outputs = model(inputs)
             loss = loss_fn(outputs, labels)
 
+            # update averages with batch
+            losses.update(loss.item(), inputs.size(0))
+
+            acc1, acc5 = accuracy(outputs, labels, topk=(1, 5))
+
+            top1.update(acc1.item(), inputs.size(0))
+            top5.update(acc5.item(), inputs.size(0))
+
             optimizer.zero_grad()
-
             loss.backward()
-
             optimizer.step()
 
-            # LOGGING
-            running_loss += loss.item()
-            minibatch_loss_list.append(loss.item())
             if not i_batch % logging_interval:
-                print('***Epoch: %03d/%03d | Batch:%04d/%04d | Loss: %.3f' % (
-                    epoch + 1, epochs, i_batch, len(train_loader), loss.item()))
-                if writer:
-                    # ...log the running loss
-                    writer.add_scalar('Running Loss/train',
-                                      running_loss / logging_interval,
-                                      global_step=epoch * len(train_loader) + i_batch)
-                    writer.add_figure('predictions vs. actuals',
-                                      plot_classes_preds(model, inputs[:4], labels[:4],[0,1,2,3,4,5,6,7,8,9]),
-                                      global_step=epoch * len(train_loader) + i_batch)
-                    weight_histograms(writer,epoch*len(train_loader)+i_batch,model)
-                running_loss = 0.0
+                pbar.set_postfix(batch=i_batch, loss=losses.avg, acc1=top1.avg, acc5=top5.avg)
+                pbar.update(logging_interval)
+
+    return losses.avg, top1.avg, top5.avg
 
 
-        with torch.no_grad():
-            train_acc, train_loss = evaluate(model, train_loader, device, loss_fn)
-            print('***Epoch: %03d/%03d | Train. Acc.: %.3f%% | Loss: %.3f' % (
-                epoch + 1, epochs, train_acc, train_loss))
-            valid_acc, valid_loss = evaluate(model, valid_loader, device, loss_fn)
-            print(f'Epoch: {epoch + 1:03d}/{epochs:03d} '
-                  f'| Train accuracy: {train_acc :.2f}% '
-                  f'| Validation accuracy: {valid_acc :.2f}% '
-                  f'| Train loss: {train_loss :.3f}'
-                  f'| Validation loss: {valid_loss :.3f}'
-                  f'| Best Validation '
-                  f'(Ep. {best_epoch:03d}): {best_valid_acc :.2f}%')
-            train_acc_list.append(train_acc)
-            valid_acc_list.append(valid_acc)
-
-            if valid_acc > best_valid_acc:
-                best_valid_acc, best_epoch = valid_acc, epoch + 1
-                if checkpoint_path:
-                    torch.save(model.state_dict(), checkpoint_path)
-
-        print('Time elapsed: %.2f min' % ((time.time() - start_time) / 60))
-
-        if scheduler is not None:
-            scheduler.step()
-        if writer:
-            writer.add_scalar('Loss/train', train_loss, epoch)
-            writer.add_scalar('Loss/test', valid_loss, epoch)
-            writer.add_scalar('Accuracy/train', train_acc, epoch)
-            writer.add_scalar('Accuracy/test', valid_acc, epoch)
-
-    print('Total Training Time: %.2f min' % ((time.time() - start_time) / 60))
-    return minibatch_loss_list, train_acc_list, valid_acc_list
-
-
-def evaluate(model, data_loader, device, loss_fn, topk=(1,)):
+def evaluate(model, data_loader, device, loss_fn):
     model.eval()
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
     with torch.no_grad():
-        topk_correct, num_examples = torch.zeros(len(topk)), 0
-        curr_loss = 0.
         for inputs, labels in data_loader:
             inputs = inputs.to(device)
             labels = labels.to(device)
 
             outputs = model(inputs)
 
-            # calculate topk predictions
-            for i, k in enumerate(topk):
-                _, predicted = torch.topk(outputs, k, 1)
-                # update running loss and correct predictions count
-                topk_correct[i] += torch.eq(labels[:, None].expand_as(predicted), predicted).any(dim=1).sum().item()
+            acc1, acc5 = accuracy(outputs, labels, (1, 5))
 
-            # calculate loss
-            loss = loss_fn(outputs, labels, reduction='sum')
-            curr_loss += loss
-            num_examples += labels.size(0)
+            # calculate loss (without reduction since it is calculated in AvgMeter)
+            loss = loss_fn(outputs, labels)
 
-    topk_acc = topk_correct / num_examples * 100
-    if len(topk_acc == 1):
-        topk_acc = topk_acc.item()
-    else:
-        topk_acc = topk_acc.numpy().tolist()
+            losses.update(loss.item(), inputs.size(0))
+            top1.update(acc1[0], inputs.size(0))
+            top5.update(acc5[0], inputs.size(0))
 
-    curr_loss = curr_loss / num_examples
-
-    return topk_acc, curr_loss.item()
+    return losses.avg, top1.avg, top5.avg
 
 
-class MyMultiStepLR(object):
-    def __init__(self, lr_dict):
-        self.lr_dict = lr_dict
-        self.curr_lr = lr_dict[0]
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
 
-    def get_lr(self, epoch):
-        if (epoch) in self.lr_dict:
-            self.curr_lr = self.lr_dict[epoch]
-        return self.curr_lr
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
 
-def main(args):
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def train(args):
     # retrieve parameters
+
     dataset = args.dataset
     seed = args.seed
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     log_interval = args.log_interval
-    checkpoint_path = args.save_path
+    save_path = pl.Path(args.save_path if args.save_path else CHECKPOINT_DIR)
+
+    if save_path.is_dir():
+        name = '%s-%s-%s' % (dataset, args.model, timestamp)
+    else:
+        name = save_path.stem
+        save_path = save_path.parent
+
+    save_path = pl.Path.joinpath(save_path, name + '.pth')
 
     set_all_seeds(seed)
     if args.tb:
-        writer = SummaryWriter('runs/%s/%s/%s' % (dataset, args.model, timestamp))
+        writer = SummaryWriter('%s/%s/base/%s/%s' % (RUNS_DIR, dataset, args.model, timestamp))
     else:
         writer = None
 
@@ -163,36 +211,52 @@ def main(args):
     # Try to get GPU device
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     if dataset == 'mnist':
-        transform = transforms.Compose(
-            [transforms.ToTensor(),
-             transforms.Normalize((0.5), (0.5))])
         classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        train_loader,test_loader = get_mnist_loader(args.batch_size,resize=args.model=='lenet- 5')
-        
+        if args.val_split:
+            assert 0 < args.val_split < 1
+            train_loader, val_loader, test_loader = get_mnist_loader(args.batch_size, val_split=args.val_split
+                                                                     , resize=args.model == 'lenet-5')
+        else:
+            train_loader, test_loader = get_mnist_loader(args.batch_size
+                                                         , resize=args.model == 'lenet-5')
+
     elif dataset == 'cifar':
-        train_loader,test_loader = get_cifar_loader(args.batch_size)
+        train_loader, test_loader = get_cifar_loader(args.batch_size)
 
     if args.model == 'lenet-300':
         model = LeNet300(10)
     elif args.model == 'lenet-5':
         model = LeNet5(10)
+
+    if writer:
+        images, labels = next(iter(train_loader))
+        writer.add_graph(model, images)
+
     model = model.to(device)
 
     lr = args.lr
     momentum = args.momentum if args.momentum else 0
     weight_decay = args.wd if args.wd else 0
 
-    if args.optimizer=='sgd':
-        optimizer = optim.SGD(model.parameters(),lr=lr,momentum=momentum, weight_decay=weight_decay)
-    elif args.optimizer == 'adam':
-        optimizer = optim.Adam(model.parameters(),lr=lr, weight_decay=weight_decay)
+    optimizer = getattr(optim, args.optimizer)(model.parameters(), lr=lr, weight_decay=weight_decay)
+
     scheduler = None
     if args.decay_step != 0:
         scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.decay_step, gamma=args.gamma)
 
     loss_fn = F.cross_entropy
 
-    training_loop(num_epochs,model,optimizer,device,train_loader,test_loader,loss_fn,log_interval,scheduler,checkpoint_path,writer)
+    best_valid_acc = training_loop(num_epochs, model, optimizer, device, train_loader, test_loader, loss_fn,
+                                   log_interval, scheduler, save_path, writer)
+
+    # Perform pruning
+
+    if writer:
+        writer.add_hparams(
+            {'dataset': dataset, 'model': args.model, 'epochs': num_epochs, 'batch_size': args.batch_size,
+             'learning rate': lr, 'momentum': momentum, 'weight decay': weight_decay,
+             'optimizer': args.optimizer, 'decay_step': args.decay_step, 'gamma': args.gamma},
+            {'accuracy': best_valid_acc})
 
 
 if __name__ == "__main__":
@@ -200,12 +264,14 @@ if __name__ == "__main__":
                                      formatter_class=lambda prog:
                                      argparse.ArgumentDefaultsHelpFormatter(prog, max_help_position=52, width=90))
     parser.add_argument('dataset', type=str, help='Select a dataset. Available options: (mnist,cifar,mini-imagenet)')
-    parser.add_argument('model', type=str, help='Select a model. Available options: (lenet-300-100,lenet-5,alexnet,vgg)')
+    parser.add_argument('model', type=str,
+                        help='Select a model. Available options: (lenet-300-100,lenet-5,alexnet,vgg)')
+    parser.add_argument('--val_split', type=float, help='fraction of training set to be used for validation')
     parser.add_argument('--seed', type=int, default=42, help='seed')
     parser.add_argument('--epochs', type=int, default=20, help='Number of epochs')
     parser.add_argument('--batch_size', type=int, default=128, help='Size of each minibatch')
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-3)')
-    parser.add_argument('--momentum', type=float, default=0, help='momentum')
+    parser.add_argument('--momentum', type=float, help='momentum')
     parser.add_argument('--wd', type=float, default=0, help='weight decay')
     parser.add_argument('--optimizer', type=str, default='sgd', help='optimizer')
     parser.add_argument('--decay_step', type=int, default=0, help='Decays the learning rate every decay_step epochs')
@@ -214,5 +280,4 @@ if __name__ == "__main__":
     parser.add_argument('--save_path', type=str, help='Save trained model in the provided path')
     parser.add_argument('--tb', default=False, action='store_true', help='Enables TensorBoard writer')
 
-    main(parser.parse_args())
-
+    train(parser.parse_args())
