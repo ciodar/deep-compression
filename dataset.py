0434f1a509271aa27fed8aeccb6c3bc1e735e01f
ciodar <dariocioni03@gmail.com>
2023-02-12 22:02:37 +0100

    mini-imagenet dataloader

--- dataset.py
+++ dataset.py
@@ -1,8 +1,8 @@
 import json
-import os
+import os, pathlib as pl
 
 from PIL import Image
-from torch.utils.data import Dataset, SubsetRandomSampler,DataLoader
+from torch.utils.data import Dataset, SubsetRandomSampler, DataLoader
 import torchvision
 import torchvision.transforms as transforms
 import torch
@@ -10,17 +10,32 @@ import torch
 DATA_DIR = os.path.dirname(os.path.abspath(__file__)) + '/data'
 
 
+# This code is modified from https://github.com/ashok-arjun/MLRC-2021-Few-Shot-Learning-And-Self-Supervision
+class INDataset(Dataset):
+    def __init__(self, meta_path, transform):
+        with open(meta_path, 'r') as f:
+            self.data = json.load(f)
+        self.transform = transform
+
+    def __getitem__(self, i):
+        image_path = os.path.join(os.pardir,self.data['image_names'][i])
+        image = Image.open(image_path).convert('RGB')
+        image = self.transform(image)
+        label = self.data['image_labels'][i]
+        return image, label
+
+    def __len__(self):
+        return len(self.data['image_names'])
+
+
 def get_mnist_loader(batch_size, num_workers=2, val_split=None, resize=False):
     # Load datasets
 
-    trainset = torchvision.datasets.MNIST(root=DATA_DIR, train=True,
-                                          download=True)
+    train_set = torchvision.datasets.MNIST(root=DATA_DIR, train=True,
+                                           download=True)
 
-
-
-
-    mean = trainset.data.float().mean() / 255
-    std = trainset.data.float().std() / 255
+    mean = train_set.data.float().mean() / 255
+    std = train_set.data.float().std() / 255
 
     # print("Train dataset mean: %.3f, std: %.3f" % (mean, std))
 
@@ -30,22 +45,22 @@ def get_mnist_loader(batch_size, num_workers=2, val_split=None, resize=False):
         ts.append(transforms.Resize(32))
 
     transform = transforms.Compose(ts)
-    trainset.transform = transform
+    train_set.transform = transform
 
-    validset = torchvision.datasets.MNIST(root=DATA_DIR, train=True,
-                                          transform=transform)
+    valid_set = torchvision.datasets.MNIST(root=DATA_DIR, train=True,
+                                           transform=transform)
 
-    testset = torchvision.datasets.MNIST(root=DATA_DIR, train=False,
-                                         download=True, transform=transform)
+    test_set = torchvision.datasets.MNIST(root=DATA_DIR, train=False,
+                                          download=True, transform=transform)
 
-    # print((len(trainset.data) + len(testset.data)))
+    # print((len(train_set.data) + len(test_set.data)))
 
-    test_loader = DataLoader(testset, batch_size=batch_size,
+    test_loader = DataLoader(test_set, batch_size=batch_size,
                              shuffle=False, num_workers=num_workers)
 
     if val_split is not None:
 
-        train_length = len(trainset.data)
+        train_length = len(train_set.data)
 
         num = int(val_split * train_length)
         train_indices = torch.arange(0, train_length - num)
@@ -54,65 +69,65 @@ def get_mnist_loader(batch_size, num_workers=2, val_split=None, resize=False):
         train_sampler = SubsetRandomSampler(train_indices)
         valid_sampler = SubsetRandomSampler(valid_indices)
 
-        valid_loader = DataLoader(dataset=validset,
+        valid_loader = DataLoader(dataset=valid_set,
                                   batch_size=batch_size,
                                   num_workers=num_workers,
                                   sampler=valid_sampler)
 
-        train_loader = DataLoader(dataset=trainset,
+        train_loader = DataLoader(dataset=train_set,
                                   batch_size=batch_size,
                                   num_workers=num_workers,
                                   drop_last=True,
                                   sampler=train_sampler)
     else:
-        train_loader = DataLoader(trainset, batch_size=batch_size,
-                                                  shuffle=True, num_workers=num_workers)
+        train_loader = DataLoader(train_set, batch_size=batch_size,
+                                  shuffle=True, num_workers=num_workers)
         return train_loader, test_loader
 
-    return train_loader,valid_loader,test_loader
+    return train_loader, valid_loader, test_loader
 
 
 def get_cifar100_loader(batch_size, num_workers=2, val_fraction=None):
     # Load datasets
-    trainset = torchvision.datasets.CIFAR100(root=DATA_DIR, train=True,
-                                             download=True)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=num_workers)
+    train_set = torchvision.datasets.CIFAR100(root=DATA_DIR, train=True,
+                                              download=True)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                               shuffle=True, num_workers=num_workers)
 
-    mean = trainset.data.float().mean() / 255
-    std = trainset.data.float().std() / 255
+    mean = train_set.data.float().mean() / 255
+    std = train_set.data.float().std() / 255
 
     # print("Train dataset mean: %.3f, std: %.3f" % (mean, std))
 
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize(mean, std)])
-    trainset.transform = transform
+    train_set.transform = transform
 
-    testset = torchvision.datasets.CIFAR100(root=DATA_DIR, train=False,
-                                            download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                             shuffle=False, num_workers=num_workers)
+    test_set = torchvision.datasets.CIFAR100(root=DATA_DIR, train=False,
+                                             download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
+                                              shuffle=False, num_workers=num_workers)
 
-    return trainloader,testloader
+    return train_loader, test_loader
 
 
 # def get_imagenet_loader(data_path,transform):
 
+def get_imagenet_loader(root, batch_size, num_workers=2):
+    transform = transforms.Compose([transforms.Resize((64, 64)),
+                                    # transforms.RandomCrop((64, 64)),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                    ])
+    train_set = INDataset(os.path.abspath(root + '/train.json'), transform)
+    valid_set = INDataset(os.path.abspath(root + '/val.json'), transform)
+    test_set = INDataset(os.path.abspath(root + '/test.json'), transform)
 
-# This code is modified from https://github.com/ashok-arjun/MLRC-2021-Few-Shot-Learning-And-Self-Supervision
-class INDataset(Dataset):
-    def __init__(self, data_path, transform):
-        with open(data_path, 'r') as f:
-            self.data = json.load(f)
-        self.transform = transform
-
-    def __getitem__(self, i):
-        image_path = os.path.join(self.data['image_names'][i])
-        image = Image.open(image_path).convert('RGB')
-        # image = self.transform(image)
-        label = self.data['image_labels'][i]
-        return image, label
-
-    def __len__(self):
-        return len(self.data['image_names'])
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                               shuffle=True, num_workers=num_workers)
+    valid_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                               shuffle=False, num_workers=num_workers)
+    valid_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                               shuffle=False, num_workers=num_workers)
+    return train_loader, valid_loader, valid_loader
