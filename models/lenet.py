15e13fce85f692fe5a9b8608eb6d7dbd98d966a5
ciodar <dariocioni03@gmail.com>
2023-02-15 17:58:57 +0100

    Switched to functional form of lenet-5

--- models/lenet.py
+++ models/lenet.py
@@ -6,13 +6,12 @@ import torch.nn.functional as F
 class LeNet300(nn.Module):
     def __init__(self, num_classes, activation=F.relu, dropout_rate=0):
         super().__init__()
-        self.fc1 = nn.Linear(28*28, 300)
+        self.fc1 = nn.Linear(28 * 28, 300)
         self.fc2 = nn.Linear(300, 100)
         self.fc3 = nn.Linear(100, num_classes)
         self.dropout = nn.Dropout(dropout_rate)
         # activation function for hidden layers
         self.activation = activation
-
 
     def forward(self, x):
         out = torch.flatten(x, 1)
@@ -26,31 +25,28 @@ class LeNet300(nn.Module):
 
 # Defining the convolutional neural network
 class LeNet5(nn.Module):
-    def __init__(self, num_classes, grayscale=False, activation=F.relu,dropout_rate=0):
+    def __init__(self, num_classes, grayscale=False, activation=F.relu, dropout_rate=0):
         super().__init__()
         self.grayscale = grayscale
         if self.grayscale:
             in_channels = 1
         else:
             in_channels = 3
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels, 6*in_channels, kernel_size=5, stride=1, padding=0),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(6*in_channels, 16*in_channels, kernel_size=5, stride=1, padding=0),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = nn.Linear(16*5*5*in_channels, 120*in_channels)
-        self.fc1 = nn.Linear(120*in_channels, 84*in_channels)
-        self.fc2 = nn.Linear(84*in_channels, num_classes)
+
+        self.conv1 = nn.Conv2d(in_channels, 6 * in_channels, kernel_size=5, stride=1, padding=0)
+        self.conv2 = nn.Conv2d(6 * in_channels, 16 * in_channels, kernel_size=5, stride=1, padding=0)
+        self.fc = nn.Linear(16 * 5 * 5 * in_channels, 120 * in_channels)
+        self.fc1 = nn.Linear(120 * in_channels, 84 * in_channels)
+        self.fc2 = nn.Linear(84 * in_channels, num_classes)
         self.dropout = nn.Dropout(dropout_rate)
         # activation function for hidden layers
         self.activation = activation
 
     def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
+        out = self.activation(self.conv1(x))
+        out = F.max_pool2d(out, kernel_size=2, stride=2)
+        out = self.activation(self.layer2(out))
+        out = F.max_pool2d(out, kernel_size=2, stride=2)
         out = out.reshape(out.size(0), -1)
         out = self.activation(self.fc(out))
         out = self.dropout(out)
